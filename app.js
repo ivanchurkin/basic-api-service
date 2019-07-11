618ab44aa9dc1d544c83c033d545e62cc5367aa8
Ivan Churkin <bahes.in@gmail.com>
2019-07-11 16:31:00 +0300

    UPDATE main app logic

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
+const logger = require('koa-logger');
+
 const app = new Koa();
 
-async function handler(ctx) {
-  ctx.body = 'Hello World';
+function onError(err, ctx) {
+  console.log(err, ctx);
 }
 
-app.use(handler);
+app.on('error', onError);
 
-app.listen(3000);
+async function errorHandler(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
+}
+
+app.use(logger());
+
+app.use(errorHandler);
+
+const router = new Router();
+const routerDogs = new Router({
+  prefix: '/dogs'
+});
+
+require('./routes/basic')(router);
+require('./routes/dogs')(routerDogs);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(routerDogs.routes());
+app.use(routerDogs.allowedMethods());
+
+const server = app.listen(3000);
+module.exports = server;
